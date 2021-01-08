72721dda719c6c3a937769042fc57309274a7864
davidbreiksh <davidbreiksh@gmail.com>
2021-01-08 22:04:11 +0000

    done

--- 08_ExceptionsDebuggingAndTesting/SPBMetro/src/test/java/RouteCalculatorTest.java
+++ 08_ExceptionsDebuggingAndTesting/SPBMetro/src/test/java/RouteCalculatorTest.java
@@ -1,27 +1,28 @@
 import core.Line;
 import core.Station;
 import junit.framework.TestCase;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class RouteCalculatorTest extends TestCase {
     List<Station> route;
+    List<Station> firstRoute;
     List<Station> secondRoute;
+    List<Station> thirdRoute;
     RouteCalculator calc;
     List<Station> connections;
     List<Station> connectionsTwo;
-    List<Station> connectionsThree;
     StationIndex stationIndex;
 
     @Override
     protected void setUp() throws Exception {
         route = new ArrayList<>();
+        firstRoute = new ArrayList<>();
         secondRoute = new ArrayList<>();
+        thirdRoute = new ArrayList<>();
+
         connections = new ArrayList<>();
         connectionsTwo = new ArrayList<>();
-        connectionsThree = new ArrayList<>();
         stationIndex = new StationIndex();
 
         Line line1 = new Line(1, "Первая");
@@ -34,46 +35,70 @@ public class RouteCalculatorTest extends TestCase {
 
         stationIndex.addStation(new Station("Шоколадная", line1));
         stationIndex.addStation(new Station("Конфетная", line1));
+        line1.addStation(stationIndex.getStation("Шоколадная", 1));
+        line1.addStation(stationIndex.getStation("Конфетная", 1));
+
         stationIndex.addStation(new Station("Куриная", line2));
         stationIndex.addStation(new Station("Говяжья", line2));
+        line2.addStation(stationIndex.getStation("Куриная", 2));
+        line2.addStation(stationIndex.getStation("Говяжья", 2));
+
         stationIndex.addStation(new Station("Клубничная", line3));
         stationIndex.addStation(new Station("Вишневая", line3));
+        line3.addStation(stationIndex.getStation("Клубничная", 3));
+        line3.addStation(stationIndex.getStation("Вишневая", 3));
 
-        connections.add(stationIndex.getStation("Шоколадная", 1));
+        connections.add(stationIndex.getStation("Конфетная", 1));
         connections.add(stationIndex.getStation("Говяжья", 2));
 
-        connectionsTwo.add(stationIndex.getStation("Конфетная" , 1));
+        connectionsTwo.add(stationIndex.getStation("Говяжья", 2));
         connectionsTwo.add(stationIndex.getStation("Клубничная", 3));
-
-        connectionsThree.add(stationIndex.getStation("Куриная" , 2));
-        connectionsThree.add(stationIndex.getStation("Вишневая" , 3));
 
         stationIndex.addConnection(connections);
         stationIndex.addConnection(connectionsTwo);
-        stationIndex.addConnection(connectionsThree);
+
+        firstRoute.add(stationIndex.getStation("Шоколадная", 1));
+        firstRoute.add(stationIndex.getStation("Конфетная", 1));
 
         secondRoute.add(stationIndex.getStation("Шоколадная", 1));
-        // secondRoute.add(stationIndex.getStation("Конфетная"));
-        //secondRoute.add(stationIndex.getStation("Куриная"));
-        secondRoute.add(stationIndex.getStation("Говяжья" , 2));
-        secondRoute.add(stationIndex.getStation("Клубничная", 3));
-        //secondRoute.add(stationIndex.getStation("Вишневая"));
+        secondRoute.add(stationIndex.getStation("Конфетная", 1));
+        secondRoute.add(stationIndex.getStation("Говяжья", 2));
+
+        thirdRoute.add(stationIndex.getStation("Шоколадная", 1));
+        thirdRoute.add(stationIndex.getStation("Конфетная", 1));
+        thirdRoute.add(stationIndex.getStation("Говяжья", 2));
+        thirdRoute.add(stationIndex.getStation("Клубничная", 3));
+        thirdRoute.add(stationIndex.getStation("Вишневая", 3));
 
         calc = new RouteCalculator(stationIndex);
 
         super.setUp();
     }
 
-    public void testGetShortestRoute() {
+    public void testGetShortestRoute1() {
         List<Station> actual = calc.getShortestRoute(stationIndex.getStation("Шоколадная", 1),
-                stationIndex.getStation("Клубничная", 3));
+                stationIndex.getStation("Конфетная", 1));
+        List<Station> expected = firstRoute;
+        assertEquals(expected, actual);
+    }
+
+    public void testGetShortestRoute2() {
+        List<Station> actual = calc.getShortestRoute(stationIndex.getStation("Шоколадная", 1),
+                stationIndex.getStation("Говяжья", 2));
         List<Station> expected = secondRoute;
         assertEquals(expected, actual);
     }
 
+    public void testGetShortestRoute3() {
+        List<Station> actual = calc.getShortestRoute(stationIndex.getStation("Шоколадная", 1),
+                stationIndex.getStation("Вишневая", 3));
+        List<Station> expected = thirdRoute;
+        assertEquals(expected, actual);
+    }
+
     public void testCalculateDuration() {
-        double actual = RouteCalculator.calculateDuration(route);
-        double expected = 14.5;
-        assertEquals(actual, expected);
+        double actual = RouteCalculator.calculateDuration(firstRoute);
+        double expected = 2.5;
+        assertEquals(expected, actual);
     }
 }
