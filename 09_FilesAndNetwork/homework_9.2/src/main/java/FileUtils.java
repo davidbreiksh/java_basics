a1b527442f32deebccd05a00f051d9e340bffc2e
davidbreiksh <davidbreiksh@gmail.com>
2021-01-26 15:37:03 +0000

    not done

--- 09_FilesAndNetwork/homework_9.2/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.2/src/main/java/FileUtils.java
@@ -1,31 +1,25 @@
 import java.io.*;
+import java.nio.channels.SeekableByteChannel;
 import java.nio.file.*;
-import java.util.Objects;
 import java.util.stream.Stream;
 
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
+    public static <File> void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
 
         Path from = Paths.get(sourceDirectory);
         Path to = Paths.get(destinationDirectory);
 
-        File scr = new File(sourceDirectory);
-        File dir = new File(destinationDirectory);
-
-        Stream<Path> files = Files.walk(from);
-
-        if (dir.isDirectory() && Objects.requireNonNull(dir.list()).length != 0) {
+        if (Files.isDirectory(from) && Files.list(to).count() != 0) {
             System.out.println("Папка не пуста или не существует");
-        } else if (scr.isDirectory() || dir.isDirectory()) {
-            files.forEach(file -> {
-                try {
-                    Files.copy(file, to.resolve(from.relativize(file)),
-                            StandardCopyOption.REPLACE_EXISTING);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
+            return;
         }
-        files.close();
+
+        Files.walk(from).forEach(src -> {
+            try {
+                Files.copy(from, to.resolve(from.relativize(to)), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
