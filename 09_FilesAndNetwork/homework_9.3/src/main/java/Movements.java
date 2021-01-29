e1460a990b01814581c54fc0a17591646a6a2310
davidbreiksh <davidbreiksh@gmail.com>
2021-01-29 14:23:12 +0000

    done

--- 09_FilesAndNetwork/homework_9.3/src/main/java/Movements.java
+++ 09_FilesAndNetwork/homework_9.3/src/main/java/Movements.java
@@ -1,6 +1,12 @@
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.regex.Pattern;
 
 public class Movements {
 
@@ -8,44 +14,41 @@ public class Movements {
 
     String line = "";
     double sum = 0;
+    String expenseColumn;
 
     public Movements(String pathMovementsCsv) {
 
     }
 
-    public double getExpenseSum() throws IOException {
+    public double getExpenseSum() throws IOException, CsvValidationException {
 
-        BufferedReader reader = new BufferedReader(new FileReader(path));
-        reader.readLine();
+        CSVReader reader = new CSVReaderBuilder(new FileReader(path)).withSkipLines(1).build();
 
-        while ((line = reader.readLine()) != null) {
+        String[] columns;
 
-            String[] expense = line.split(",");
-
-            //if (expense[6].matches("\\d+\\.\\d+"))
-
-                double sumExpense = Integer.parseInt(expense[7].replace("\"", ""));
-
-                sum = sum + sumExpense;
-
+        while ((columns = reader.readNext()) != null) {
+            columns[7] = columns[7].replaceAll("\"", "");
+            columns[7] = columns[7].replaceAll(",", ".");
+            double sumExpense = Double.parseDouble(columns[7]);
+            sum += sumExpense;
         }
         System.out.println("Общий расход " + sum + " рублей");
         return sum;
     }
 
-    public double getIncomeSum() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(path));
-        reader.readLine();
 
-        while ((line = reader.readLine()) != null) {
-            String[] expense = line.split(",");
+    public double getIncomeSum() throws IOException, CsvValidationException {
+        CSVReader reader = new CSVReaderBuilder(new FileReader(path)).withSkipLines(1).build();
 
-            double sumExpense = Double.parseDouble(expense[6].replace("\"", ""));
+        String[] columns;
 
-            sum = sum + sumExpense;
-
+        while ((columns = reader.readNext()) != null) {
+            columns[6] = columns[6].replaceAll("\"", "");
+            columns[6] = columns[6].replaceAll(",", ".");
+            double sumExpense = Double.parseDouble(columns[6]);
+            sum += sumExpense;
         }
-        System.out.println("Общий доход " + sum + " рублей");
+        System.out.println("Общий расход " + sum + " рублей");
         return sum;
     }
 }
