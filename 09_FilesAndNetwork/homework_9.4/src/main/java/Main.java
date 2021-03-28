f64e56a36c94bf9368dd14af0b9ce832650d583b
breiksh_david <davidbreiksh@gmail.com>
2021-03-28 16:31:47 +0100

    done

--- 09_FilesAndNetwork/homework_9.4/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.4/src/main/java/Main.java
@@ -1,43 +1,66 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
+
 import java.net.URL;
 
 public class Main {
+
+    private static Logger notImageLogger;
+
+    private static String dstPath = "C:\\Users\\david\\IdeaProjects\\homework_9.4\\src\\image";
+
     public static void main(String[] args) throws IOException {
 
+        notImageLogger = LogManager.getLogger("NotImage");
+
         String webSite = "http://lenta.ru/";
-        String dstPath = "C:\\Users\\david\\IdeaProjects\\homework_9.4\\src\\image";
 
 
         Document doc = Jsoup.connect(webSite).get();
         Elements img = doc.select("img");
 
-        for (Element links : img) {
-            String st = links.attr("src");
-            String fileName = st.substring(st.lastIndexOf('/') + 1);
-            System.out.println(st);
 
-            URL url = new URL(st);
-            InputStream it = new BufferedInputStream(url.openStream());
+        try {
 
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-
-            byte[] buf = new byte[1024];
-            int n = 0;
-            while (-1 != (n = it.read(buf))) {
-                os.write(buf, 0, n);
+            for (Element links : img) {
+                String st = links.attr("src");
+                downloadImg(st);
+                System.out.println(st);
             }
-            it.close();
-            os.close();
-            byte[] response = os.toByteArray();
-
-            FileOutputStream fs = new FileOutputStream(dstPath + "\\" + fileName);
-            fs.write(response);
-            fs.close();
+        } catch (Exception e) {
+            notImageLogger.info("Не картинка");
         }
+    }
+
+    public static void downloadImg(String imgLink) throws IOException {
+        URL url = new URL(imgLink);
+        inStream(url, imgLink);
+    }
+
+    public static void inStream(URL url, String imgName) throws IOException {
+        String fileName = imgName.substring(imgName.lastIndexOf('/') + 1);
+
+        InputStream it = new BufferedInputStream(url.openStream());
+
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        byte[] buf = new byte[1024];
+        int n = 0;
+        while (-1 != (n = it.read(buf))) {
+            os.write(buf, 0, n);
+        }
+        it.close();
+        os.close();
+        byte[] response = os.toByteArray();
+
+        FileOutputStream fs = new FileOutputStream(dstPath + "\\" + fileName);
+        fs.write(response);
+        fs.close();
     }
 }
